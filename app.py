4b10d1ec2c9dc589b6920a8555f1c4aafde1869a
KDM777 <daemingim5@gmail.com>
2024-08-06 10:46:13 +0900

    modify app.py

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ def load_dataset(chatBotType):
     # chatBotType에 따라 다른 CSV 파일을 로드합니다.
     csv_files = {
         "아저씨": "oldman_dataset.csv",
-        "아줌마": "wellness_dataset.csv"
+        "아줌마": "oldgirl_dataset.csv"
         # 필요한 다른 타입의 데이터셋 파일들을 여기에 추가
     }
     
     # 기본값은 '아저씨'로 설정
-    csv_file = csv_files.get(chatBotType, "wellness_dataset.csv")
+    csv_file = csv_files.get(chatBotType, "oldman_dataset.csv")
     
     # 데이터셋 로드
     df = pd.read_csv(csv_file)
